9df5dc9c91fb33600bad05d679798f5011786935
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-21 17:00:12 +0530

    Add files via upload

---
+++ error_hadling.py
@@ -0,0 +1,91 @@
+''' part-1 '''
+# print("python")
+# print("core python")
+# try:
+#     print(5/2)
+# except:
+#     print('you have divided by zero')  
+#     print("advance python")
+#     print("python Analytics")  
+
+'''part-2 '''
+# print("python")
+# print("core python")
+# try:
+#     print(10/2)
+# except:
+#     print('not ok  divided by zero')  
+#     print("advance python")
+#     print("python Analytics")  
+
+''' part-3 '''
+
+# print("python")
+# print("core python")
+# a=10
+# try:
+#     print(b)
+# except ZeroDivisionError:
+#      print('not ok  divided by zero')  
+# print("advance python")
+# print("python Analytics")  
+
+''' part-4'''
+
+# print("python")
+# print("core python")
+# a=10
+# try:
+#     print(a/0)
+#     print(b)
+# except ZeroDivisionError:
+#      print('not ok  divided by zero')  
+# print("advance python")
+# print("python Analytics")  
+
+'''' part-5 '''
+# print("python")
+# print("core python")
+# a=8
+# b=0
+# try:
+#     print(a/4)
+#     print(b)
+# except ZeroDivisionError:
+#     print("not ok divided zero")
+# print("advance python")
+# print("python Analystic")
+
+'''part-6 '''
+# print("python")
+# print("core python")
+# a=8
+# try:
+#     print(a/4)
+#     print(b)
+# except ZeroDivisionError:
+#     print("not ok divided zero")
+# except NameError:
+#     print('you have not ok defind the name')    
+# print("advance python")
+# print("python Analystic")
+
+'''' part-7 '''
+print("python")
+print("core python")
+a=8
+try:
+    print(a/4)
+    print(a)
+    print(a/0)
+except ZeroDivisionError:
+    print("not ok divided zero") 
+print("advance python")
+print("python Analystic")
+
+
+
+
+
+
+
